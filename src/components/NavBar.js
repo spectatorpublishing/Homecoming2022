d4a5ecce208ac4947cdf1c2e6ba02fdbe45fbda4
Daniel Jeong <sdjeong0404@gmail.com>
2022-10-19 21:04:51 -0400

    made navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,20 +8,44 @@ import theme from '../theme';
 const Container = styled.div`
     display: flex;
     flex-direction: column;
+    justify_content: space-between;
     position: absolute;
-    width: 8rem;
     margin-right:2.5rem;
     top: 0;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
     right: 0;
+    background-color: ${theme.colors.lightBlue};
     @media only screen and (max-width: 768px){
     }
 `;
 
+const Lion = styled.div`
+    content: ${theme.lion.roarie};
+`
+
 const MenuItem = styled.div`
-    padding: 0.5rem 1rem;
+    width:100%;
+    padding: 0.5rem 0.5rem 1rem 1rem;
+    margin:auto;
+    font-family: 'IBM Plex Serif';
+    font-style: normal;
+    font-size: 1em;
+    color:${theme.colors.lightBlue};
+    text-align:left;
+    text-decoration:none;
+    transition-duration:0.5s;
     a {
         text-decoration: none;
-        color: ${theme.colors.black};
+        color: ${theme.colors.blueText};
+    }
+    :hover {
+        background-color:${theme.colors.darkBlue};
+        color:${theme.colors.white};
+    }
+    @media (max-width: 768px) {
+        font-size:1.5em;
+        width:80%;
     }
 `;
 
@@ -31,6 +55,7 @@ const NavBar = ({ }) => {
             {sections.map(section => (
                 <MenuItem>
                     <Link to={section.url}>{section.title}</Link>
+                    <Lion></Lion>
                 </MenuItem>
             ))}
         </Container>
--- src/data/sections.js
+++ src/data/sections.js
@@ -5,32 +5,16 @@ export const sections = [
         exact: true,
     },
     {
-        title: "University News",
-        url: "/uni-news",
-    },
-    {
-        title: "City News",
-        url: "/city-news",
+        title: "Sports",
+        url: "/sports",
     },
     {
         title: "A&E",
         url: "/a&e",
     },
     {
-        title: "Sports",
-        url: "/sports",
-    },
-    {
         title: "Opinion",
         url: "/opinion",
-    },
-    {
-        title: "The Eye",
-        url: "/eye",
-    },
-    {
-        title: "Illustrations",
-        url: "/illo",
     },
     {
         title:"Acknowledgements",
