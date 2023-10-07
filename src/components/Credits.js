6b2e42a56add0c561b74acbe1d9dceb525be6961
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-10-07 16:14:11 -0400

    Credits task

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,7 +4,7 @@ import { credits } from '../data/credits'
 
 const Container = styled.div`
     width: 100%;
-    background-color: #D2E1EE;
+    background-color: #9FB3BF;
     color: #0046A6;
     padding-bottom:3rem;
 `;
@@ -17,22 +17,24 @@ const HeaderDiv = styled.div`
 const Header = styled.h1`
     text-align: center;
     width: 70%;
-    font-size: 2.5rem;
-    font-family: 'IBM Plex Serif', 'Georgia', Times, serif;
+    font-size: 2.188rem;
+    font-family: 'Kalam', cursive;
+    font-weight: 400;
     padding-top:2rem;
+    color: #000000;
 
     @media only screen and (max-width: 1216px){
-        font-size: 2.1rem;
+        font-size: 2.0rem;
     }
 
     @media only screen and (max-width: 768px){
         width: 80%;
-        font-size: 2rem;
+        font-size: 1.8rem;
     }
 
     @media only screen and (max-width: 600px){
         font-weight: 400;
-        font-size: 1.3rem;
+        font-size: 1.6rem;
         padding-top:5rem;
     }
 `;
@@ -43,14 +45,14 @@ const SectAndNames = styled.div`
     flex-wrap: wrap;
     align-content:space-around;
     width: 100%;
-    height: 150rem;
+    height: 160rem;
     margin-top: 2rem;
 
     @media only screen and (max-width: 1216px){
         height: 160rem;
     }
     @media only screen and (max-width: 1024px){
-        height: 125rem;
+        height: 160rem;
     }
 
     @media only screen and (max-width: 768px){
@@ -64,63 +66,67 @@ const SectAndNames = styled.div`
 `;
 
 const Section = styled.div`
-    font-size: 1.8rem;
-    font-weight: 700;
+    font-size: 1.25rem;
+    font-weight: 400;
     width: 22rem;
     height: fit-content;
     margin-bottom: 2rem;
     padding:0;
     margin:0;
-    font-family:  'IBM Plex Serif', 'Georgia', Times, serif;
+    font-family: 'Special Elite', cursive;
+    color: #ffffff;
 
     @media only screen and (max-width: 1216px){
-        font-size: 1.6rem;
+        font-size: 1.0rem;
         width: 16rem;
     }
 
     @media only screen and (max-width: 1024px){
-        font-size: 1.4rem;
+        font-size: 1.0rem;
         width: 14rem;
     }
 
     @media only screen and (max-width: 768px){
-        font-size: 1.5rem;
+        font-size: 1.25rem;
         text-align: center;
         width: 100%;
     }
     @media only screen and (max-width: 600px){
-        font-size: 1.2rem;
+        font-size: 1.0rem;
         text-align: center;
         width: 100%;
     }
 `;
 
 const Name = styled.div`
-    font-family: 'Roboto', sans-serif;
-    font-size: 1.15rem;
+    font-family: 'Special Elite', cursive;
+    font-size: 1.0rem;
     font-weight: 400;
+    height: 2rem;
     margin-top: 1rem;
     margin-bottom: 1rem;
+    color: #000000;
 
     @media only screen and (max-width: 1216px){
-        font-size: 1rem;
+        font-size: 0.9rem;
     }
     @media only screen and (max-width: 1024px){
-        font-size: 0.75rem;
+        font-size: 0.9rem;
     }
     @media only screen and (max-width: 768px){
-        font-size:1rem;
+        font-size: 1.0rem;
     }
 `;
 
 const Credits = () => {
     return (
         <Container id="credits">
-            <HeaderDiv><Header>THE COLUMBIA DAILY SPECTATOR STAFF WHO MADE THIS ISSUE POSSIBLE</Header></HeaderDiv>
+            <HeaderDiv><Header>Staff Who Made This Issue Possible</Header></HeaderDiv>
             <SectAndNames>
                 {Object.entries(credits).map(([section, key]) => {
                     return(
                         <Section> {section} 
+                        <br/>
                         <br/>
                         {key.map((person, index) => {
                             return(
